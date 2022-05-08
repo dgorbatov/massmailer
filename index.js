ae0dd1bcd923c482e0e704aaa7166d93fdec1598
Daniel Gorbatov <dg20@uw.edu>
2022-05-08 00:28:07 -0700

    finish

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 import {readFileSync} from "fs";
+import {createTransport} from "nodemailer";
+import "dotenv/config";
 
-const data = readFileSync('./twb2.json',
-            {encoding:'utf8', flag:'r'});
+const tutor_subject = "";
+const tutor_message = "";
+const student_subject = "";
+const student_message = "";
 
-// Display the file data
-console.log(data);
+const data = JSON.parse(readFileSync('./twb2.json',
+            {encoding:'utf8', flag:'r'}));
+
+try {
+  let transporter = createTransport({
+    host: "p3plzcpnl475181.prod.phx3.secureserver.net",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.USER, // generated ethereal user
+      pass: process.env.PASS, // generated ethereal password
+    },
+  });
+
+  // Student emails
+  for (let i of data[2].data) {
+    let info = await transporter.sendMail({
+      from: 'info@educationwithoutboarders.com', // sender address
+      to: i.email, // list of receivers
+      subject: student_subject, // Subject line
+      text: student_message, // plain text body
+    });
+  }
+
+
+  // Teacher emails
+  for (let i of data[3].data) {
+    let info = await transporter.sendMail({
+      from: 'info@educationwithoutboarders.com', // sender address
+      to: i.email, // list of receivers
+      subject: tutor_subject, // Subject line
+      text: tutor_message, // plain text body
+    });
+  }
+
+
+} catch(err) {
+  console.error(err);
+}
